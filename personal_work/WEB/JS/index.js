40b47f352692b18e7adf5bbc2df86dfc407f0dde
James <jnjai8821@gmail.com>
2022-07-06 11:18:22 +0300

    index.html

---
+++ personal_work/WEB/JS/index.js
@@ -0,0 +1,12 @@
+
+//single line comment in Javascript
+/*
+Multi-line comment in Javascript
+*/
+let num1 = 134
+let num2 = 134
+console.log(num1 + num2)
+
+function submitButton(){
+    alert("Button has been clicked")
+}
